fbc94eae162b54e14c68e5a266fc34cf315fa3de
Simran Kaur <kaursim.2019@yahoo.com>
2021-07-27 23:41:43 -0700

    updates

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,3 +2,11 @@ USE employee_db;
 
 INSERT INTO department (name) VALUES 
 ("stuff");
+
+
+INSERT INTO role (title, salary, department_id) VALUES
+("engineer", "140000.00", 1);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+("simran", "kaur", 1, 1);
--- server.js
+++ server.js
@@ -32,15 +32,50 @@ function mainMenu() {
     .then((response) => {
         switch (response.choices) {
             case 'view all departments':
-                db.query('SELECT * FROM department;')
+                db.query('SELECT * FROM department;', function (err, results) {
+                    console.log(results);})
                 break;
             case 'view all roles':
-                db.query('SELECT title FROM role;')
+                db.query('SELECT role.id, role.title, role.salary, department.name FROM role JOIN department ON department_id=department.id;', function (err, results) {
+                    console.log(results);})
                 break;
             case 'view all employees':
-                db.query('SELECT first_name last_name FROM employee;')
+                db.query('SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name, employee.manager_id FROM employee JOIN role ON role_id=role.id JOIN department ON department_id=department.id;', function (err, results) {
+                    console.log(results);})
                 break;
-            // case 'add a department':
+            case 'add a department':
+                inquirer
+                    .prompt(
+                        {
+                        type:'input',
+                        message: "What department would you like to add?",
+                        name: 'department'
+                        })
+                db.query('INSERT INTO department (name) VALUE ', function (err, results) {
+                    console.log(results);})
+                break;
+            case 'add a role':
+                inquirer
+                    .prompt(
+                        {
+                        type:'input',
+                        message: "What role would you like to add?",
+                        name: 'role'
+                        })
+                db.query('INSERT INTO role (title) VALUE ', function (err, results) {
+                    console.log(results);})
+                break;
+            case 'add an employee':
+                inquirer
+                    .prompt(
+                        {
+                        type:'input',
+                        message: "What employee would you like to add?",
+                        name: 'employee'
+                        })
+                db.query('INSERT INTO employee (first_name) VALUE ', function (err, results) {
+                    console.log(results);})
+                break;
 
         }
     });
